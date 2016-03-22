d312ad53e0d3116d973783349ca0140f851abc4a
SalvatierraFran <franco14_93@hotmail.com>
2016-03-22 11:44:56 -0300

    Primer Commit. Casi terminado. Clase 2 de programación.

---
+++ Calculadora.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClaseDosProgramacion2_01
+{
+    class Calculadora
+    {
+        /// <summary>
+        /// Este metodo debera sumar dos numeros.
+        /// </summary>
+        private static void Sumar(double Numero1, double Numero2)
+        {
+            double resultado;
+            Console.Write("Estoy sumando. ");
+            resultado = Numero1 + Numero2;
+            Console.WriteLine("El resultado es " + resultado);
+            //Console.ReadKey();
+        }
+
+        private static void Restar(double Numero1, double Numero2)
+        {
+            Console.Write("Estoy restando. ");
+            double resultado;
+            resultado = Numero1 - Numero2;
+            Console.WriteLine("El resultado es " + resultado);
+            //Console.ReadKey();
+        }
+
+        private static void Multiplicar(double Numero1, double Numero2)
+        {
+            Console.Write("Estoy multiplicando. ");
+            double resultado;
+            resultado = Numero1 * Numero2;
+            Console.WriteLine("El resultado es " + resultado);
+            //Console.ReadKey();
+        }
+
+        private static void Dividir(double Numero1, double Numero2)
+        {
+            if (Calculadora.ValidarSiEsDistintoACero(Numero2) == true)
+            {
+                Console.Write("Estoy dividiendo. ");
+                double resultado;
+                resultado = Numero1 / Numero2;
+                Console.WriteLine("El resultado es " + resultado);
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+            //Console.ReadKey();
+        }
+
+        private static bool ValidarSiEsDistintoACero(double NumeroUno)
+        {
+            bool rta;
+
+            if (NumeroUno != 0)
+            {
+                rta = true;
+            }
+            else
+            {
+                rta = false;
+            }
+
+            return rta;
+        }
+
+        public static void Calcular(double NumeroUno, double NumeroDos, string Operacion)
+        {
+
+            switch (Operacion)
+            {
+                case "1":
+                    Calculadora.Sumar(NumeroUno, NumeroDos);
+                    Console.ReadKey();
+                    break;
+
+                case "2":
+                    Calculadora.Restar(NumeroUno, NumeroDos);
+                    Console.ReadKey();
+                    break;
+
+                case "3":
+                    Calculadora.Multiplicar(NumeroUno, NumeroDos);
+                    Console.ReadKey();
+                    break;
+
+                case "4":
+                    Calculadora.Dividir(NumeroUno, NumeroDos);
+                    Console.ReadKey();
+                    break;
+            }
+        }
+
+        public static double Mayor(double NumUno, double NumDos, double NumTres)
+        {
+            double max;
+
+            if (NumUno > NumDos && NumUno > NumTres)
+            {
+                max = NumUno;
+            }
+
+            else if (NumDos > NumUno && NumDos > NumTres)
+            {
+                max = NumDos;
+            }
+
+            else
+            {
+                max = NumTres;
+            }
+
+            return max;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClaseDosProgramacion2_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculadora.Sumar(5, 5);
+            //Calculadora.Restar(10, 8);
+            //Calculadora.Multiplicar(5, 6);
+            //Calculadora.Dividir(6, 0);
+
+            //Console.Write(Calculadora.ValidarSiEsDistintoACero(0));
+
+            Console.WriteLine("Menu de calculadora");
+            Console.WriteLine("1.Suma\n2.resta\n3.Multiplicacion\n4.Division");
+
+            Calculadora.Calcular(5, 6, "4");
+
+            Console.WriteLine(Calculadora.Mayor(5, 6, 8));
+
+            Console.ReadKey();
+        }
+    }
+}
